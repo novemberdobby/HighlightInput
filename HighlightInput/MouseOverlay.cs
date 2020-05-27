e20dc45a15c1560c578cf4cab79ad62ae4cb4c1a
Unknown <novemberdobby@gmail.com>
2020-05-27 20:31:43 +0100

    different colours per button renamed some vars

--- HighlightInput/MouseOverlay.cs
+++ HighlightInput/MouseOverlay.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace HighlightInput
@@ -6,23 +7,32 @@ namespace HighlightInput
     class MouseOverlay : IDisposable
     {
         #region Tweakables
-        readonly int c_mouseRadius = 35;
-        readonly float c_mouseInitialOpacity = 0.4f;
-        readonly double c_mouseFadeMillis = TimeSpan.FromSeconds(0.4).TotalMilliseconds;
-        readonly float c_mouseBorderWidth = 0f;
+        readonly int c_radius = 35;
+        readonly float c_initialOpacity = 0.4f;
+        readonly double c_fadeMillis = TimeSpan.FromSeconds(0.4).TotalMilliseconds;
+        readonly float c_borderWidth = 0f;
+
+        readonly Dictionary<MouseButtons, GameOverlay.Drawing.Color> c_colours = new Dictionary<MouseButtons, GameOverlay.Drawing.Color>()
+        {
+            { MouseButtons.Left, new GameOverlay.Drawing.Color(1f, 1f, 0f) },
+            { MouseButtons.Right, new GameOverlay.Drawing.Color(0.7f, 0f, 0f) },
+            { MouseButtons.Middle, new GameOverlay.Drawing.Color(0f, 0.5f, 1) },
+        };
         #endregion
 
         #region State
-        private System.Drawing.Point m_mousePos;
-        private DateTime m_mouseDownTime = DateTime.MinValue;
-        private bool m_mouseIsDown;
+        private System.Drawing.Point m_position;
+        private DateTime m_downTime = DateTime.MinValue;
+        private bool m_isDown;
+        private MouseButtons m_button;
+
         private GameOverlay.Windows.GraphicsWindow m_overlay;
         #endregion
 
         public MouseOverlay()
         {
             //window must be big enough to accomodate the circle + border, add a little extra to help with clipping
-            int windowSize = (int)((c_mouseRadius + c_mouseBorderWidth) * 2.1f);
+            int windowSize = (int)((c_radius + c_borderWidth) * 2.1f);
             m_overlay = new GameOverlay.Windows.GraphicsWindow(0, 0, windowSize, windowSize);
             m_overlay.DrawGraphics += DrawGraphics;
             m_overlay.Create();
@@ -32,25 +42,26 @@ namespace HighlightInput
 
         public void MouseDown(MouseEventArgs args)
         {
-            m_mousePos.X = args.X;
-            m_mousePos.Y = args.Y;
-            m_mouseDownTime = DateTime.Now;
-            m_mouseIsDown = true;
+            m_button = args.Button;
+            m_position.X = args.X;
+            m_position.Y = args.Y;
+            m_downTime = DateTime.Now;
+            m_isDown = true;
         }
 
         public void MouseMove(MouseEventArgs args)
         {
             //if clicked, update the position
-            if(m_mouseIsDown)
+            if(m_isDown)
             {
-                m_mousePos.X = args.X;
-                m_mousePos.Y = args.Y;
+                m_position.X = args.X;
+                m_position.Y = args.Y;
             }
         }
 
         public void MouseUp(MouseEventArgs args)
         {
-            m_mouseIsDown = false;
+            m_isDown = false;
         }
 
         public void Dispose()
@@ -63,29 +74,35 @@ namespace HighlightInput
             var window = sender as GameOverlay.Windows.GraphicsWindow;
 
             //if down, reset the time to keep it at max opacity
-            if (m_mouseIsDown)
+            if (m_isDown)
             {
-                m_mouseDownTime = DateTime.Now;
+                m_downTime = DateTime.Now;
             }
 
-            double millisElapsed = DateTime.Now.Subtract(m_mouseDownTime).TotalMilliseconds;
-            if (millisElapsed > c_mouseFadeMillis)
+            double millisElapsed = DateTime.Now.Subtract(m_downTime).TotalMilliseconds;
+            if (millisElapsed > c_fadeMillis)
             {
                 return;
             }
 
             //centre overlay on the click
-            window.Move(m_mousePos.X - window.Width / 2, m_mousePos.Y - window.Height / 2);
+            window.Move(m_position.X - window.Width / 2, m_position.Y - window.Height / 2);
 
             e.Graphics.ClearScene();
 
-            float mouseAlpha = c_mouseInitialOpacity * (1.0f - (float)(millisElapsed / c_mouseFadeMillis));
-            e.Graphics.FillCircle(e.Graphics.CreateSolidBrush(1, 1, 0, mouseAlpha), new GameOverlay.Drawing.Circle(window.Width / 2, window.Height / 2, c_mouseRadius));
+            GameOverlay.Drawing.Color curColour;
+            if(!c_colours.TryGetValue(m_button, out curColour))
+            {
+                curColour = c_colours[MouseButtons.Left];
+            }
+
+            float mouseAlpha = c_initialOpacity * (1.0f - (float)(millisElapsed / c_fadeMillis));
+            e.Graphics.FillCircle(e.Graphics.CreateSolidBrush(curColour.R, curColour.G, curColour.B, mouseAlpha), new GameOverlay.Drawing.Circle(window.Width / 2, window.Height / 2, c_radius));
 
             //if there's a border, render just outside the radius
-            if (c_mouseBorderWidth > 0)
+            if (c_borderWidth > 0)
             {
-                e.Graphics.DrawCircle(e.Graphics.CreateSolidBrush(0, 0, 0, mouseAlpha), new GameOverlay.Drawing.Circle(window.Width / 2, window.Height / 2, c_mouseRadius + c_mouseBorderWidth / 2.0f), c_mouseBorderWidth);
+                e.Graphics.DrawCircle(e.Graphics.CreateSolidBrush(0, 0, 0, mouseAlpha), new GameOverlay.Drawing.Circle(window.Width / 2, window.Height / 2, c_radius + c_borderWidth / 2.0f), c_borderWidth);
             }
         }
     }
