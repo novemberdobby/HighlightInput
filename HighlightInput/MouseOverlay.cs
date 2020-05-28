dc10f2eff91e7f791d541ee55a8b590026139285
Unknown <novemberdobby@gmail.com>
2020-05-28 19:10:03 +0100

    mouse overlay now supports: -animating the radius from 0 to target size -antialiasing

--- HighlightInput/MouseOverlay.cs
+++ HighlightInput/MouseOverlay.cs
@@ -8,9 +8,10 @@ namespace HighlightInput
     {
         #region Tweakables
         readonly int c_radius = 35;
-        readonly float c_initialOpacity = 0.4f;
-        readonly double c_fadeMillis = TimeSpan.FromSeconds(0.4).TotalMilliseconds;
-        readonly float c_borderWidth = 0f;
+        readonly float c_initialOpacity = 0.5f;
+        readonly double c_fadeMillis = TimeSpan.FromSeconds(0.5).TotalMilliseconds;
+        readonly double c_growMillis = TimeSpan.FromSeconds(0.1).TotalMilliseconds;
+        readonly float c_borderWidth = 4f;
 
         readonly Dictionary<MouseButtons, GameOverlay.Drawing.Color> c_colours = new Dictionary<MouseButtons, GameOverlay.Drawing.Color>()
         {
@@ -33,8 +34,9 @@ namespace HighlightInput
         {
             //window must be big enough to accomodate the circle + border, add a little extra to help with clipping
             int windowSize = (int)((c_radius + c_borderWidth) * 2.1f);
-            m_overlay = new GameOverlay.Windows.GraphicsWindow(0, 0, windowSize, windowSize);
+            m_overlay = new GameOverlay.Windows.GraphicsWindow(0, 0, windowSize, windowSize, new GameOverlay.Drawing.Graphics() { PerPrimitiveAntiAliasing = true, TextAntiAliasing = true });
             m_overlay.DrawGraphics += DrawGraphics;
+            m_overlay.FPS = 60;
             m_overlay.Create();
             m_overlay.IsTopmost = true;
             m_overlay.Show();
@@ -52,7 +54,7 @@ namespace HighlightInput
         public void MouseMove(MouseEventArgs args)
         {
             //if clicked, update the position
-            if(m_isDown)
+            if (m_isDown)
             {
                 m_position.X = args.X;
                 m_position.Y = args.Y;
@@ -63,47 +65,67 @@ namespace HighlightInput
         {
             m_isDown = false;
         }
-
-        public void Dispose()
-        {
-            m_overlay.Dispose();
-        }
-
+        
         private void DrawGraphics(object sender, GameOverlay.Windows.DrawGraphicsEventArgs e)
         {
-            var window = sender as GameOverlay.Windows.GraphicsWindow;
+            e.Graphics.ClearScene();
 
             //if down, reset the time to keep it at max opacity
             if (m_isDown)
             {
-                m_downTime = DateTime.Now;
+                if(c_growMillis > 0)
+                {
+                    //if grow is enabled, only reset to max grow size - not initial click size
+                    double elapsed = DateTime.Now.Subtract(m_downTime).TotalMilliseconds;
+                    if(elapsed > c_growMillis)
+                    {
+                        m_downTime = DateTime.Now.Subtract(TimeSpan.FromMilliseconds(c_growMillis));
+                    }
+                }
+                else
+                {
+                    m_downTime = DateTime.Now;
+                }
             }
 
             double millisElapsed = DateTime.Now.Subtract(m_downTime).TotalMilliseconds;
-            if (millisElapsed > c_fadeMillis)
+            if (millisElapsed > c_growMillis + c_fadeMillis)
             {
                 return;
             }
 
             //centre overlay on the click
-            window.Move(m_position.X - window.Width / 2, m_position.Y - window.Height / 2);
+            m_overlay.Move(m_position.X - m_overlay.Width / 2, m_position.Y - m_overlay.Height / 2);
 
-            e.Graphics.ClearScene();
+            float alpha = c_initialOpacity;
+            float radius = c_radius;
+
+            if (c_growMillis > 0 && millisElapsed <= c_growMillis) //in grow stage
+            {
+                radius *= (float)(millisElapsed / c_growMillis);
+            }
+            else //past grow stage
+            {
+                alpha = c_initialOpacity * (1.0f - (float)((millisElapsed - c_growMillis) / c_fadeMillis));
+            }
 
             GameOverlay.Drawing.Color curColour;
-            if(!c_colours.TryGetValue(m_button, out curColour))
+            if (!c_colours.TryGetValue(m_button, out curColour))
             {
                 curColour = c_colours[MouseButtons.Left];
             }
-
-            float mouseAlpha = c_initialOpacity * (1.0f - (float)(millisElapsed / c_fadeMillis));
-            e.Graphics.FillCircle(e.Graphics.CreateSolidBrush(curColour.R, curColour.G, curColour.B, mouseAlpha), new GameOverlay.Drawing.Circle(window.Width / 2, window.Height / 2, c_radius));
+            e.Graphics.FillCircle(e.Graphics.CreateSolidBrush(curColour.R, curColour.G, curColour.B, alpha), new GameOverlay.Drawing.Circle(m_overlay.Width / 2, m_overlay.Height / 2, radius));
 
             //if there's a border, render just outside the radius
             if (c_borderWidth > 0)
             {
-                e.Graphics.DrawCircle(e.Graphics.CreateSolidBrush(0, 0, 0, mouseAlpha), new GameOverlay.Drawing.Circle(window.Width / 2, window.Height / 2, c_radius + c_borderWidth / 2.0f), c_borderWidth);
+                e.Graphics.DrawCircle(e.Graphics.CreateSolidBrush(0, 0, 0, alpha), new GameOverlay.Drawing.Circle(m_overlay.Width / 2, m_overlay.Height / 2, radius + c_borderWidth / 2.0f), c_borderWidth * (radius / c_radius));
             }
+        }
+
+        public void Dispose()
+        {
+            m_overlay.Dispose();
         }
     }
 }
